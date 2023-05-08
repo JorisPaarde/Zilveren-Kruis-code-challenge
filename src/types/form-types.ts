93d2a60ea7e32600f73ad8939e9f3d5917adc7ae
JorisPaarde <jorispaarde@gmail.com>
2023-05-08 12:57:16 +0200

    Moves types folder

---
+++ src/types/form-types.ts
@@ -0,0 +1,9 @@
+import type { AanvullendeVerzekeringOption } from 'src/interfaces/interfaces';
+
+export const AanvullendeVerzekeringOptions: AanvullendeVerzekeringOption[] = [
+    { naam: 'Geen aanvullende verzekering geselecteerd', prijs: '€ 0,00' },
+    { naam: 'Aanvullend 1', prijs: '€ 21,38 per jaar' },
+    { naam: 'Aanvullend 2', prijs: '€ 85,06 per jaar' },
+    { naam: 'Aanvullend 3', prijs: '€ 198,63 per jaar' },
+    { naam: 'Aanvullend 4', prijs: '€ 359,73 per jaar' },
+  ];
